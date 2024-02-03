f9bd57234b4d23567267595d1cf5d0fc592b2ac2
Ronan Twomey Friedlander <97160203+Ronantfs@users.noreply.github.com>
2024-02-03 13:04:17 +0000

    Add files via upload
    new updates ; encapsulation of functionlaity ; 
    now compling records of pre paid blocks of lesson ; months now ordered so can do cumulative calculations across months ; generate invoice function now also fed compiled pre paid blocks

---
+++ main.py
@@ -0,0 +1,189 @@
+#TODO refector this project to use the new structure where control flow is in main.py and functions are in other files
+import pandas as pd
+import gspread
+import numpy as np
+
+import tutor_hours
+import generate_invoices
+#import update_invoice_records
+import constants
+import utils
+import data_checks
+
+
+
+#fucntionality to update invoice records:
+
+class InvoiceManager:
+    def __init__(self):
+        self.all_students_unpaid_monthly_dfs = None
+        self.all_students_unpaid_monthly_dfs_pre_paid = None
+        self.all_students_prepaid_blocks = None
+
+        self.students_pre_paid_hours_used = {}
+        self.students_hourly_totals = {}
+
+        #self.all_invoice_records_df = None
+
+
+    def update_invoice_records(self):
+        # invoice records
+        all_tutors_lesson_log_df = tutor_hours.get_all_tutors_lesson_log_df(constants.list_of_ut_tutors)
+        # master data
+        mastersheet_data = tutor_hours.get_master_data()
+        mastersheet_data_df = mastersheet_data[0]
+        UT_Master_Data_Base_Sheet = mastersheet_data[1]
+
+        #update_master_record
+        tutor_hours.add_new_tutor_lessons_to_master(mastersheet_data_df,
+                                                     all_tutors_lesson_log_df,
+                                                     UT_Master_Data_Base_Sheet)
+
+    def compile_unpaid_monthly_records(self):
+        '''Sets self.all_students_unpaid_monthly_dfs to a df of all unpaid records:
+        unpaid records: dict[student: monthly dfs of unpaid records]'''
+
+        # compile unpaid records into df to then be used to create invoices
+        dfs: tuple = tutor_hours.process_master_data_to_df()
+        self.all_students_unpaid_monthly_dfs = dfs[0]
+        self.all_students_unpaid_monthly_dfs_pre_paid = dfs[1]
+        self.all_students_prepaid_blocks = dfs[2]
+
+        if self.all_students_unpaid_monthly_dfs is not None:
+            print("Hourly DataFrames are available for use.")
+            print(self.all_students_unpaid_monthly_dfs.keys())
+
+
+        if self.all_students_unpaid_monthly_dfs_pre_paid is not None:
+            print("Pre paid DataFrames are available for use.")
+            print(self.all_students_unpaid_monthly_dfs_pre_paid.keys())
+
+    def generate_invoice_records(self, invoice_type: str):
+        '''Generates PDF invoices of unpaid hours
+
+        If invoice type == pre-paid, then the function will also sum the total pre-paid hours used for each student,
+        and store this in self.students_pre_paid_hours_used'''
+
+        if invoice_type not in ['hourly', 'prepaid']:
+            print("Invalid invoice type specified.")
+            return
+
+
+        #define set of student dfs to use to generate invoices
+        if invoice_type == 'hourly':
+            if self.all_students_unpaid_monthly_dfs is None:
+                print(
+                    "DataFrame for hourly invoices is not available. Please run the appropriate option to compile first.")
+                return
+            student_dfs = self.all_students_unpaid_monthly_dfs
+            print("\nGenerating unpaid invoices\n...")
+
+        elif invoice_type == 'prepaid':
+            if self.all_students_unpaid_monthly_dfs_pre_paid is None:
+                print("DataFrame for pre-paid invoices is not available. Please run the appropriate option to compile first.")
+                return
+            student_dfs = self.all_students_unpaid_monthly_dfs_pre_paid
+            print("\nGenerating pre-paid invoices\n...")
+
+        #generate invoices for each student
+        for student in student_dfs:
+            print(f'\nprocessing {student} invoices... Generating PDFs and summing any prepaid hours used.')
+
+
+
+            # Process the invoice data
+            processed_data, students_total_hours = generate_invoices.process_invoice_data(student,
+                                                                                          student_dfs[student],
+                                                                                          invoice_type)
+
+            ordered_invoices = utils.sort_invoices_chronologically(processed_data)
+
+            # Initialize the data structure
+            all_invoice_data = {
+                'invoices': ordered_invoices
+            }
+
+            # Add prepaid blocks data if invoice type is 'prepaid'
+            if invoice_type == 'prepaid':
+                all_invoice_data['prepaid_blocks'] = self.all_students_prepaid_blocks.get(student, pd.DataFrame())
+
+            # Generate the invoice PDFs
+            generate_invoices.generate_invoice_pdfs(all_invoice_data, student, invoice_type)
+
+            #set invoice records
+            if invoice_type == 'prepaid':
+                self.students_pre_paid_hours_used[student] = students_total_hours
+            if invoice_type == 'hourly':
+                self.students_hourly_totals[student] = students_total_hours
+
+
+    def function_4(self):
+        pass
+
+    def run(self):
+        '''Control flow for the program'''
+
+        while True:
+            print('\nSelect a function to run:')
+            print('1. Centralise all tutor hours onto master (cloud) [Get new tutor hours]')
+            print('2. Compile unpaid monthly records to class variable')
+            print('3. Generate invoices and set invoice records') #TODO add processing for pre paid invoices
+            print('4. Check student balances')
+            print('5. Exit')
+
+            try:
+                choice = int(input("Enter an appropriate integer: "))
+                if choice == 1:
+                    self.update_invoice_records()
+
+                elif choice == 2:
+                    self.compile_unpaid_monthly_records()
+                    data_checks.check_invoice_master_data()
+                    print(0)
+
+                elif choice == 3:
+
+                    # Early exit if the data checks fail
+                    if not (data_checks.check_invoice_master_data() and data_checks.check_student_tutor_names_unique()):
+                        print(
+                            "DataFrame is not available. ")
+                        return
+
+                    # Check if both DataFrames are None
+                    if self.all_students_unpaid_monthly_dfs is None and self.all_students_unpaid_monthly_dfs_pre_paid is None:
+                        print("No unpaid invoices available for processing. If there are unpaid invoice, Please run option 2 to compile df from google sheets first.")
+                        return
+
+                    # Process only hourly rate invoices
+                    if self.all_students_unpaid_monthly_dfs is not None:
+                        self.generate_invoice_records('hourly')
+                        # If prepaid invoices are also available, process them
+                        if self.all_students_unpaid_monthly_dfs_pre_paid is not None:
+                            self.generate_invoice_records('prepaid')
+
+                elif choice == 4:
+
+                    if self.students_pre_paid_hours_used or self.students_hourly_totals:
+                        print('Prepaid hours log:',self.students_pre_paid_hours_used)
+                        print('Hourly totals log:',self.students_hourly_totals)
+                        gross_hourly_money = sum(self.students_hourly_totals.values())
+                        print('Gross hourly money:', gross_hourly_money)
+
+                    else:
+                        print('')
+                        print("Pre-paid hours have not been calculated yet. Please run option 3 first.")
+
+                elif choice == 5:
+                    print("Exiting program.")
+                    break
+                else:
+                    print('Not a valid choice')
+
+            except ValueError:
+                print("Invalid input. Please enter an integer.")
+
+if __name__ == '__main__':
+    manager = InvoiceManager()
+    manager.run()
+
+
--- update_invoice_records.py
+++ update_invoice_records.py
@@ -3,7 +3,7 @@ import gspread
 import numpy as np
 
 # Import the necessary module that contains the function to generate the invoice data DataFrame
-import invoice_csv_to_pdf
+import generate_invoices
 
 invoice_csv_to_pdf.main()
 new_invoice_records_df = invoice_csv_to_pdf.all_invoice_records_df
@@ -26,6 +26,7 @@ def add_new_invoice_records_to_master():
     mastersheet_data_df['Permanent ID'] = mastersheet_data_df['Permanent ID'].astype(str)
     new_invoice_records_df['Permanent ID'] = new_invoice_records_df['Permanent ID'].astype(str)
 
+    #TODO FIX this as permanet ID not unqiue if two studnets with same first name (see Finn J )
     new_rows = new_invoice_records_df[~new_invoice_records_df['Permanent ID'].isin(mastersheet_data_df['Permanent ID'])]
     updated_mastersheet_data_df = pd.concat([mastersheet_data_df, new_rows])
 
@@ -40,3 +41,4 @@ def add_new_invoice_records_to_master():
 
 add_new_invoice_records_to_master()
 print('new invoice records added')
+
